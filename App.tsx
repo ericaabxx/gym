ce5e5daa2f1e999b350a50ce5cb9dc0d8c2251ea
ericaabxx <contatoericadasilvaabreu@gmail.com>
2025-04-11 06:54:24 -0300

    second commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+ import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
+ import { config } from './config/gluestack-ui.config'
+ import { Loading } from './source/components/Loading'
+ import { SignUp } from '@screens/SignUp'
+ 
+ export default function App() {
+   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+ 
+   return (
+     <GluestackUIProvider config={config}>
+       <StatusBar
+         barStyle="light-content"
+         backgroundColor="transparent"
+         translucent
+       />
+       {fontsLoaded ? <SignUp /> : <Loading />}
+     </GluestackUIProvider>
+   )
+ }
---
+++ config/theme/AccordionHeader.ts
@@ -0,0 +1,1 @@
+// Componente de tema: AccordionHeader.ts
